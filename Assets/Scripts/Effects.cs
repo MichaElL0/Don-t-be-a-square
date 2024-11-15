90439b037e77a609dd3b87a84fffc47de3a6e7df
mik <54302018+MichaElL0@users.noreply.github.com>
2024-11-15 11:30:55 +0100

    Add music and polish

--- Assets/Scripts/Effects.cs
+++ Assets/Scripts/Effects.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,16 +7,6 @@ public class Effects : MonoBehaviour
 {
 	[Header("Effects")]
 	public UnityEvent Effect;
-	//public RectTransform react;
-
-	private Vector2 startPosition;
-	private float startTime;
-
-	private void Start()
-	{
-		//startPosition = react.anchoredPosition;
-		startTime = Time.time;
-	}
 
 	// Update is called once per frame
 	void Update()
@@ -32,13 +24,5 @@ public class Effects : MonoBehaviour
 	{
 		float angle = 1 * Mathf.Sin(Time.time * speed);
 		transform.localRotation = Quaternion.Euler(0, 0, angle);
-	}
-
-	public void WaveEffect(float speed)
-	{
-		float elapsedTime = Time.time - 1;
-		float newX = startPosition.x + elapsedTime * 1 * 10;
-		float sineAmmount = startPosition.y + 10 * Mathf.Sin((elapsedTime * 1) + (Time.time * 1));
-		//react.anchoredPosition = new Vector2(newX, sineAmmount);
 	}
 }
